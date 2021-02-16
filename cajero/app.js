b792a6f005a2abc81cdc1bc3dac4a83b5bcb34c7
pablofg89 <pablo_fg89@hotmail.com>
2021-02-15 21:37:38 -0600

    updated

--- cajero/app.js
+++ cajero/app.js
@@ -1,7 +1,7 @@
 //Validacion de los inputs
 
-var usuario = document.getElementById('usuario').value;
-var password = document.getElementById('password').value;
+var usuario = document.getElementById('usuario');
+var password = document.getElementById('password');
 
 let error_usuario = document.getElementById('error_usuario');
 let error_password = document.getElementById('error_password');
@@ -24,18 +24,18 @@ var cuentas  = [{
 ];
 
 function validacion() {
-    // if(usuario.value.length < 3){
-    //     usuario.style.border = "1px solid red";
-    //     error_usuario.style.display= "block";
-    //     usuario.focus();
-    //     return false;
-    // }
-    // if(password.value.length < 3){
-    //     password.style.border = "1px solid red";
-    //     error_password.style.display= "block";
-    //     password.focus();
-    //     return false;
-    // }
+    if(usuario.value.length < 3){
+        usuario.style.border = "1px solid red";
+        error_usuario.style.display= "block";
+        usuario.focus();
+        return false;
+    }
+    if(password.value.length < 3){
+        password.style.border = "1px solid red";
+        error_password.style.display= "block";
+        password.focus();
+        return false;
+    }
     for (let i = 0; i < cuentas.length ; i++){
         let element = cuentas[i];
         if((element.nombre == usuario) && (element.password == password)){
