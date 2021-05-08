de01e849a2fbdccec30e3f7578fdbdac75d272a3
pablofg89 <pablo_fg89@hotmail.com>
2021-05-07 21:54:42 -0600

    actualizacion de css

--- pokeapi/app.js
+++ pokeapi/app.js
@@ -1,46 +1,5 @@
-//const axios = require("axios");
 
-//GET
-// let obtenerPokemones = (cantidad) => {
-//     const URL = `https://pokeapi.co/api/v2/pokemon?limit=${cantidad}`;
-//     axios
-//         .get(URL)
-//         .then(response => {
-//             //Valida que se tenga respuesta.
-//             if (response.status === 200) {
-//                 //Recore el listado de la respuesta donde me muesta Nombre y URL
-//                 let listado = response.data.results;
-//                 for (let i = 0; i < listado.length; i++) {
-//                     //Recoro el arreglo para el URL
-//                     axios.get(listado[i].url)
-//                         .then(response => {
-//                             // Guardo el nombre para crea el objeto del pokemon
-//                             const pokemon = {
-//                                 name:listado[i].name,
-//                                 tipo:[]
-//                             }
-//                             //Muestra la informacion del pokemon todo el api
-//                             let infoApi = response.data;
-
-//                             let pokemonType = response.data.types;
-//                             //Buscar el tipo del pokemon
-//                             for( let i = 0; i< pokemonType.length; i++){
-
-//                                 pokemon.tipo.push(pokemonType[i].type.name)
-//                             }
-//                             console.log(pokemon);
-
-//                         })
-//                         .catch((error) => console.log(error));
-//                 }
-//             }
-//         })
-//         .catch((error) => console.log(error));
-// };
-// obtenerPokemones(10);
-let allPokemons= [];
-
-//GET
+let allPokemons = [];
 let obtenerPokemones = (cantidad) => {
   const URL = `https://pokeapi.co/api/v2/pokemon?limit=${cantidad}`;
   let arrayPokemones = [];
@@ -74,19 +33,19 @@ let obtenerPokemones = (cantidad) => {
 let creatCards = (pokemones) => {
   let contenedorPokemones = document.getElementById("pokemons-container");
 
-  pokemones.forEach(pokemon => {
+  pokemones.forEach((pokemon) => {
     let card = document.createElement("div");
     let titulo = document.createElement("h5");
-    let descripcion= document.createElement("p");
+    let descripcion = document.createElement("p");
     let imagen = document.createElement("img");
 
-    card.setAttribute("class","card h-100 d-flex flex-nowrap");
+    card.setAttribute("class", "card h-100 d-flex flex-nowrap");
     card.style.width = "10rem";
 
-    imagen.setAttribute("class","card-img-top");
+    imagen.setAttribute("class", "card-img-top");
 
-    titulo.setAttribute("class","card-title");
-    descripcion.setAttribute("class","card-text");
+    titulo.setAttribute("class", "card-title");
+    descripcion.setAttribute("class", "card-text");
     //Definir orden de los elementos
     contenedorPokemones.append(card);
     card.append(imagen, titulo, descripcion);
@@ -98,10 +57,11 @@ let creatCards = (pokemones) => {
   });
 };
 
-
 let buscar = () => {
-    let busqueda = document.getElementById("valor").value;
+  let busqueda = document.getElementById("valor").value;
 
-    let resultados = allPokemons.filter(pokemon =>pokemon.nombre.includes(busqueda) );
-    console.log(resultados);
-}
+  let resultados = allPokemons.filter((pokemon) =>
+    pokemon.nombre.includes(busqueda)
+  );
+  console.log(resultados);
+};
